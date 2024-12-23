906bcd4f7d648ed4775490bc1ee53af100259766
ki2183 <ki2183@naver.com>
2024-12-23 14:17:53 +0900

    feat(component/loginModal): 전역 상태로 로그인 모달 관리 기능 구현
    - Zustand를 활용한 로그인 모달 전역 상태 관리 로직 추가
    - 로그인 모달 UI와 전역 상태 연동
    
    Resolves: #34

--- src/app/_components/LoginModal/LoginModal.client.tsx
+++ src/app/_components/LoginModal/LoginModal.client.tsx
@@ -1,15 +1,29 @@
+"use client"
+
 import Image from "next/image";
 import CzzSVG from "@public/loginModal/Logo.svg";
 import CloseSVG from "@public/loginModal/close.svg";
+import useLoginModalToggle from "@/app/_store/modal/useLoginModalToggle.cliet";
 
 const LoginModal = () => {
+    const { isFold, toggle } = useLoginModalToggle();
 
+    if(!isFold) return null;
+
+    // before선 스타일
     const beforeLine = "before:content-[''] before:block before:w-full before:h-[0.5px] before:bg-[#66666671] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2"
 
+    const onClickKakaoButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log('로그인 api? url')
+    }
+
     return (
-        <div id="login-modal">
-            <div className="fixed z-[20000] w-screen h-screen top-0 left-0 backdrop-blur-[2px] bg-[#252525c4] flex items-center justify-center">
-                <div className="relative">
+            <div
+                id="login-modal"  
+                onClick={toggle} 
+                className="fixed z-[20000] select-none  w-screen h-screen top-0 left-0 backdrop-blur-[2px] bg-[#252525c4] flex items-center justify-center"
+            >
+                <div className="relative" onClick={e => e.stopPropagation()}>
                     <div 
                         id="login-layer" 
                         style={{scrollbarWidth:"none"}} 
@@ -19,12 +33,10 @@ const LoginModal = () => {
                             <CzzSVG width={74} height={26} className="mx-auto scale-150"/>
                         </div>
 
-                        <div className="p-[9px_12px] text-[11px] text-center mt-[8px]">
+                        <div aria-label="로그인 설명" className="p-[9px_12px] text-[11px] text-center mt-[8px]">
                             로그인하여 간단하게 스트리밍을 시작하세요! <br/>
                             또는 자신이 좋아하는 스트리머를 팔로우하고 <br/>
                             댓글을 달아보세요!
-                            
-                            
                         </div>
 
                         <div className={`text-[13px] w-full text-center relative h-[24px] mt-[6px] ${beforeLine}`}>
@@ -33,22 +45,23 @@ const LoginModal = () => {
                             </span>
                         </div>
 
-                        <div className="mt-[16px]">
-                            <button className="w-[95%] text-[14px] mx-auto p-[10px_12px] border-[1px] rounded-[6px] text-[#343434e5] border-solid border-[#6666662d] flex items-center shadow-sm bg-white hover:brightness-[90%]">
-                                <Image alt="로그인 버튼" width={20} height={20} src="/loginModal/kakao.webp" className="rounded-full mr-[8px]"/>
-                                <span className="font-[600]">
-                                    카카오 간편 로그인
-                                </span>
-                            </button>  
-                        </div>
+                        <ol aria-label="소셜 로그인 모음" className="mt-[16px] flex flex-col gap-[12px]">
+                            <li aria-label="카카오 로그인">
+                                <button onClick={onClickKakaoButton} className="w-[95%] text-[14px] mx-auto p-[10px_12px] border-[1px] rounded-[6px] text-[#343434e5] border-solid border-[#6666662d] flex items-center shadow-sm bg-white hover:brightness-[90%]">
+                                    <Image alt="로그인 버튼" width={20} height={20} src="/loginModal/kakao.webp" className="rounded-full mr-[8px]"/>
+                                    <span className="font-[600]">
+                                        카카오 간편 로그인
+                                    </span>
+                                </button>  
+                            </li>
+                        </ol>
 
-                        <button className="absolute scale-75 top-0 right-0 m-[12px_12px_0_0] bg-white hover:brightness-90">
+                        <button onClick={toggle} className="absolute scale-75 top-0 right-0 m-[12px_12px_0_0] bg-white hover:brightness-90">
                             <CloseSVG width={24} height={24} className="scale-75"/>
                         </button>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
