fec05d08168344eba8afaa296e6730dfa9219d5e
yungan9 <geyun105@naver.com>
2024-12-24 02:16:12 +0900

    chore(channelPage): 프로필 사진 크기 고정

--- src/app/(route)/[channel]/components/ChannelProfile.tsx
+++ src/app/(route)/[channel]/components/ChannelProfile.tsx
@@ -9,7 +9,7 @@ const ChannelProfile: React.FC<ChannelProfileProps> = ({ nickname, follower, con
   return (
     <div className="flex items-start justify-between max-w-7xl p-4 rounded-lg m-2 mt-32">
       <div className="flex items-center"> 
-        <div className="w-24 h-24 rounded-full bg-white shadow-md mr-4"/> 
+        <div className="w-24 h-24 rounded-full flex-shrink-0 bg-white shadow-md mr-4"/> 
         <div className="flex-grow flex flex-col justify-center"> 
           <p className="text-2xl font-black">{nickname}</p>
           <p className="text-lg font-semibold text-gray-500">팔로워 {follower}만 명</p>
