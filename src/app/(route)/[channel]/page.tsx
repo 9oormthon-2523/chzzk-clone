8327d309bf1b44978d959b0c3ccb7adb741fef4a
yungan9 <geyun105@naver.com>
2024-12-24 14:18:35 +0900

    wip(channelPage): 채널 페이지 글 데이터 불러오기 구현중 - 데이터 빈 배열로 불러오는 오류

--- src/app/(route)/[channel]/page.tsx
+++ src/app/(route)/[channel]/page.tsx
@@ -1,8 +1,36 @@
+'use client';
+
+import { useState, useEffect } from "react"
+import {createClient} from "../../_utils/supabase/client";
 import ChannelProfile from "./components/ChannelProfile"
 import Post from "./components/Post"
 import BoardInput from "./components/BoardInput"
 
 const Channel = () => {
+  const [posts, setPosts] = useState<{ nickname: string; content: string }[]>([]);
+  useEffect(() => {
+    console.log("데이터 불러오기");
+    const fetchPosts = async () => {
+      try {
+        const supabase = createClient();  
+        const { data, error } = await supabase
+          .from('posts') 
+          .select('nickname, content');
+
+        console.log("잘 되는중", data);
+        console.log('error:', error);
+
+        if (error) throw error; 
+        setPosts(data || []);
+      } catch (error) {
+        console.error('데이터 불러오기 실패', error);
+      }
+    };
+
+
+    fetchPosts(); 
+  }, []);
+
   return (
     <>
       <div className="ml-12 mr-12">
@@ -10,9 +38,9 @@ const Channel = () => {
           <p className="text-xl font-black ml-4 mt-6">커뮤니티</p>
           <BoardInput/>
           <div className="flex items-start flex-col max-w-7xl bg-white rounded-lg m-2 mt-6 gap-6">
-            <Post nickname="엄청난 물고기" content="배부른 물고기가 느리다."/>
-            <Post nickname="엄청난 물고기" content="물고기 또는 어류는 척추동물아문에 속하는 동물의 하나다. 또한 척추동물 중에서 네발동물은 제외된다. 대체적으로 물 속에 살며 아가미로 호흡하며 지느러미로 움직이고 몸 표면이 비늘로 덮여 있는 냉혈동물로 주위 온도에 영향을 받는다."/>
-            <Post nickname="엄청난 물고기" content="아쿠아리움 물고기는 억지로 잡아왔나요? 잘 모르시나 보네요 물고기들이 모집 요강 보고 지원해서 이력서 쓰고 면접보고 뽑습니다."/>
+          {posts.map((post, id) => (
+            <Post key={id} nickname={post.nickname} content={post.content} />
+          ))}
           </div>
         <div className="h-32"/>
       </div>
