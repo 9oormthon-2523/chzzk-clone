a9ac0c01bd783cef4f89de5fd680f8f9b2035944
yungan9 <geyun105@naver.com>
2024-12-26 06:05:19 +0900

    feat(channelPage): 채널 페이지 프로필 UI 구현 - 채널 프로필 컴포넌트 분리

---
+++ src/app/(route)/channel/[uid]/components/ChannelProfile.tsx
@@ -0,0 +1,26 @@
+
+interface ChannelProfileProps {
+  nickname: string;
+  follower: number;
+  context: string;
+}
+
+const ChannelProfile: React.FC<ChannelProfileProps> = ({ nickname, follower, context }) => {
+  return (
+    <div className="flex items-start justify-between max-w-7xl p-4 rounded-lg m-2 mt-32">
+      <div className="flex items-center"> 
+        <div className="w-24 h-24 rounded-full flex-shrink-0 bg-white shadow-md mr-4"/> 
+        <div className="flex-grow flex flex-col justify-center"> 
+          <p className="text-2xl font-black">{nickname}</p>
+          <p className="text-lg font-semibold text-gray-500">팔로워 {follower}만 명</p>
+          <p className="text-lg font-medium text-gray-400">{context}</p>
+        </div>
+      </div>
+      <button className="w-20 h-8 bg-[#1bb373] rounded-full flex items-center justify-center text-white font-black">
+        팔로우
+      </button>
+    </div>
+  );
+}
+
+export default ChannelProfile;
