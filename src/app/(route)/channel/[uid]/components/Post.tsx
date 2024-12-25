6c4cc67eb490fd9f0df8a1e30995193ee1a08722
yungan9 <geyun105@naver.com>
2024-12-26 06:02:10 +0900

    feat(channelPage): 채널 페이지 글 컴포넌트 구현 - Post 컴포넌트 분리 - 글 작성자, 내용 불러오기

---
+++ src/app/(route)/channel/[uid]/components/Post.tsx
@@ -0,0 +1,20 @@
+interface PostProps {
+  nickname: string;
+  content: string;
+}
+
+const Post = (props: PostProps) => {
+  const { nickname, content } = props;
+
+  return (
+    <div className="flex w-full h-48 p-4 bg-gray-50 rounded-lg ">
+          <div className="flex flex-col flex-shrink-0 w-12 h-12 rounded-full bg-white shadow-md text-sm mr-2"/>
+          <div className="flex flex-col flex-wrap">
+            <p className="text-m font-black">{nickname}</p>
+            <div className="text-m w-full h-32 overflow-hidden break-words mt-2">{content}</div>
+          </div>
+        </div>
+  )
+}
+
+export default Post;
