14deab8f7b7cbf00e12cb6a654e0316c434b2bc8
donggyun <donggyunkim12@gmail.com>
2024-12-25 23:30:07 +0900

    feat(studioPage): 라우트 uid 값 서버검증 로직 구현
    - isMatchUid 를 통해 라우트와 supabase.auth.getUser 의 id 값을 비교하여 페이지 보호
    - layout.tsx 서버컴포넌트 전환으로 인한 ContentWrapper 구현
    
    Resolves: #27

--- src/app/(route)/studio/[uid]/(route)/layout.tsx
+++ src/app/(route)/studio/[uid]/(route)/layout.tsx
@@ -1,27 +1,49 @@
-'use client';
-
 import React, { ReactNode } from 'react';
 import Header from '../_components/Header/Header.server';
 import Navigation from '../_components/Navigation/Navigation.client';
-import useNavSizeToggle from '@/app/_store/studio/useNavSizeToggle.client';
 import StudioWrapper from '../_components/common/StudioWrapper.server';
+import ContentWrapper from '../_components/common/ContentWrapper.client';
+import { createClient } from '@/app/_utils/supabase/server';
+import { redirect } from 'next/navigation';
+import Popup from '../_components/Popup/Popup.server';
 
-const StudioLayout = ({ children }: { children: ReactNode }) => {
-  const { isFold } = useNavSizeToggle();
-  return (
-    <div>
-      <Header />
-      <div
-        className="flex pt-[61px] bg-[#f1f3f5] transition-all duration-200 ease-in-out"
-        style={{
-          paddingLeft: isFold ? '69px' : '240px',
-        }}
-      >
-        <Navigation />
-        <StudioWrapper>{children}</StudioWrapper>
+const StudioLayout = async ({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: Promise<{ uid: string }>;
+}) => {
+  const { uid } = await params;
+  const match = await isMatchUid(uid);
+
+  if (match) {
+    return (
+      <div>
+        <Header />
+        <ContentWrapper>
+          <Navigation />
+          <StudioWrapper>{children}</StudioWrapper>
+        </ContentWrapper>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <Popup />;
 };
 
 export default StudioLayout;
+
+const isMatchUid = async (uid: string) => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return false;
+  if (!uid) redirect(`/studio/${user.id}`);
+  if (user.id !== uid) return false;
+
+  return true;
+};
