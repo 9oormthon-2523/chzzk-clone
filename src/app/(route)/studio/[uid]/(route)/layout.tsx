fe46a261174fbe074cac1d335e6b2ad82a6b9018
donggyun <donggyunkim12@gmail.com>
2024-12-29 17:55:18 +0900

    feat(studioPage): 사용자의 로그인 상황과 라우트에 따른 studioPage 접근 UI 조건부 렌더링 구현 #49

--- src/app/(route)/studio/[uid]/(route)/layout.tsx
+++ src/app/(route)/studio/[uid]/(route)/layout.tsx
@@ -3,8 +3,8 @@ import Header from '../_components/Header/Header.server';
 import Navigation from '../_components/Navigation/Navigation.client';
 import ContentWrapper from '../_components/common/ContentWrapper.client';
 import { createClient } from '@/app/_utils/supabase/server';
-import { redirect } from 'next/navigation';
 import Popup from '../_components/Popup/Popup.server';
+import Block from '../_components/Block/Block.server';
 
 const StudioLayout = async ({
   children,
@@ -14,37 +14,36 @@ const StudioLayout = async ({
   params: Promise<{ uid: string }>;
 }) => {
   const { uid } = await params;
-  const match = await isMatchUid(uid);
+  const match = await matchStatus(uid);
 
-  if (match) {
-    return (
-      <div className="fixed w-full h-full">
-        <Header uid={uid} />
-        <ContentWrapper>
-          <Navigation uid={uid} />
-          <section className="flex overflow-auto flex-1 flex-col bg-[#f1f3f5] h-full">
-            {children}
-          </section>
-        </ContentWrapper>
-      </div>
-    );
-  }
+  if (match === 'noUser') return <Popup />;
+  if (match === 'notMatch') return <Block uid={uid} />;
 
-  return <Popup />;
+  return (
+    <div className="fixed w-full h-full">
+      <Header uid={uid} />
+      <ContentWrapper>
+        <Navigation uid={uid} />
+        <section className="flex overflow-auto flex-1 flex-col bg-[#f1f3f5] h-full">
+          {children}
+        </section>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default StudioLayout;
 
-const isMatchUid = async (uid: string) => {
+const matchStatus = async (
+  uid: string
+): Promise<'noUser' | 'notMatch' | 'match'> => {
   const supabase = await createClient();
 
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user) return false;
-  if (!uid) redirect(`/studio/${user.id}`);
-  if (user.id !== uid) return false;
-
-  return true;
+  if (!user) return 'noUser';
+  if (user.id !== uid) return 'notMatch';
+  return 'match';
 };
