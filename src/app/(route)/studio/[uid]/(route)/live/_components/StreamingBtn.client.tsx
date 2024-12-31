2ce19ee42acb8d1a1dc064c581059308b849f861
ki2183 <ki2183@naver.com>
2024-12-31 14:17:09 +0900

    feat(studioPage/[uid]/live): 동적 라우팅 개선
    - StreamingCoreBtn: 실질적인 스트리밍 기능을 담당하는 컴포넌트로 변경
    - StreamingBtn: 동적 라우팅을 처리하는 컴포넌트로 역할 분리
    - 컴포넌트 명확성을 위해 역할에 따라 이름 변경
    
    Resolve: #54

--- src/app/(route)/studio/[uid]/(route)/live/_components/StreamingBtn.client.tsx
+++ src/app/(route)/studio/[uid]/(route)/live/_components/StreamingBtn.client.tsx
@@ -1,18 +1,11 @@
-'use client';
+"use clinet"
 
-import React from 'react';
+import dynamic from 'next/dynamic';
+const StreamingCoreBtn = dynamic(() => import('./StreamingCoreBtn.client'), { ssr: false });
 
 const StreamingBtn = () => {
-  return (
-    <button
-      className="rounded-2xl bg-white text-[#697183] text-[15px] font-blackHanSans py-[5px] px-[15px]"
-      onClick={() => {
-        // 여기에 로직 추가하면 될듯합니다!
-      }}
-    >
-      스트리밍 시작하기
-    </button>
-  );
-};
-
-export default StreamingBtn;
+    return <>
+      <StreamingCoreBtn/>
+    </>
+}
+export default StreamingBtn
---
+++ src/app/(route)/studio/[uid]/(route)/live/_components/StreamingCoreBtn.client.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+const StreamingCoreBtn = () => {
+  return (
+    <button
+      className="rounded-2xl bg-white text-[#697183] text-[15px] font-blackHanSans py-[5px] px-[15px]"
+      onClick={() => {
+        // 여기에 로직 추가하면 될듯합니다!
+      }}
+    >
+      스트리밍 시작하기
+    </button>
+  );
+};
+
+export default StreamingCoreBtn;
