478aa3b45efab9e6b8e7fd408d6787081a421dca
ki2183 <ki2183@naver.com>
2024-12-31 21:56:37 +0900

    feat(studioPage/live/streaming): Studio 스트리밍 훅 연결 및 구조 개선
    - agora-rtc-sdk-ng 설치 및 설정
    - StreamingBtn: createClient를 이용해 호스트의 uid를 전달 받도록함
    - 기존 develope 브랜치의 훅과 머지 시 오류 가능성을 방지하기 위해 새로 디렉토리 생성 및 구조 개선
    - 오디오 믹싱 시 음질 저하 문제로 오디오 트랙 두 개를 분리하여 전송
    - 테스트용으로 버튼 추가로 생성함 기능은 아래와 같음
    
    주요 구현 기능:
    - streamOn : 호스트 클라이언트 생성 -> 채널 참가 -> 미디어 리소스 생성 -> publish
    - streamOff : 미디어 리소스 unpublish -> 미디어 리소스 off 및 제거 -> 채널 나가기 -> 클라이언트 삭제
    - addTrackShare : 스크린 + 마이크 트랙 공유, 스크린 공유 , 마이크 공유
    - stopTrackShare : 모든 미디어 리소스 off 및 해제
    
    Resolve: #54

--- src/app/(route)/studio/[uid]/(route)/live/_components/StreamingBtn.client.tsx
+++ src/app/(route)/studio/[uid]/(route)/live/_components/StreamingBtn.client.tsx
@@ -1,11 +1,31 @@
-"use clinet"
+"use client";
 
+import { createClient } from '@/app/_utils/supabase/client';
+import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
+
 const StreamingCoreBtn = dynamic(() => import('./StreamingCoreBtn.client'), { ssr: false });
 
 const StreamingBtn = () => {
-    return <>
-      <StreamingCoreBtn/>
-    </>
-}
-export default StreamingBtn
+  const supabase = createClient();
+  const [userId, setUserId] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data: user, error } = await supabase.auth.getUser();
+      if (!error && user) {
+        setUserId(user.user.id);
+      }
+      setLoading(false);
+    };
+    fetchUser();
+  }, []);
+
+  if (loading) return null;
+  
+
+  return <>{userId ? <StreamingCoreBtn uid={userId} /> : <p>사용자 정보를 불러올 수 없습니다.</p>}</>;
+};
+
+export default StreamingBtn;
