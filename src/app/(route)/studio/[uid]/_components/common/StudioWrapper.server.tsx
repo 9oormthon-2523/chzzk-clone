7b490b54cd5ba933133b967dd723ef306bddd62f
donggyun <donggyunkim12@gmail.com>
2025-01-02 18:11:09 +0900

    design: StudioWrapper 컴포넌트 w-full 추가

--- src/app/(route)/studio/[uid]/_components/common/StudioWrapper.server.tsx
+++ src/app/(route)/studio/[uid]/_components/common/StudioWrapper.server.tsx
@@ -9,8 +9,8 @@ const StudioWrapper = (props: StudioContainer) => {
   const { children } = props;
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-1 max-w-[1200px] min-w-[698px] p-[40px_50px_0] box-border m-[0_auto]">
+    <div className="flex flex-col justify-between w-full h-full">
+      <div className="flex flex-1 w-full max-w-[1200px] min-w-[698px] p-[40px_50px_0] box-border m-[0_auto]">
         {children}
       </div>
       <Footer />
