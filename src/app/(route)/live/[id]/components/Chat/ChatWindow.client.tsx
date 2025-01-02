269be630544b3c95fa87eea019eb4ea9608ca726
AhnRian <00rian@naver.com>
2025-01-02 21:01:50 +0900

    feat(livePage): chat기능 livePage 연동

--- src/app/(route)/live/[id]/components/Chat/ChatWindow.client.tsx
+++ src/app/(route)/live/[id]/components/Chat/ChatWindow.client.tsx
@@ -1,44 +1,54 @@
+import { Message } from "@/app/_types/chat/Chat";
 /**
  * 채팅 창
  */
 
 //프롭스는 나중에
+type MessageListProps = {
+  messages: Message[];
+};
 
-const ChatWindow = () => {
-
-    return (
-        <div id="chatting-list-container" className="flex flex-[1_1] overflow-hidden relative">
-            <div style={{ scrollbarWidth:"none" }} className="flex flex-col overflow-y-auto p-[0_12px] w-full">
-                {/* 채팅 */}
-                <div id="empty-box-forChat" aria-label="빈 박스"/>
-                <div className="text-[14px]">
-                    {
-                        Array.from({length:45}).map((_, idx)=>(
-                            <ChatBox key={idx}/>
-                        ))
-                    }
-                </div>
-                
-            </div>
+const ChatWindow = ({ messages }: MessageListProps) => {
+  return (
+    <div
+      id="chatting-list-container"
+      className="flex flex-[1_1] overflow-hidden relative"
+    >
+      <div
+        style={{ scrollbarWidth: "none" }}
+        className="flex flex-col overflow-y-auto p-[0_12px] w-full"
+      >
+        {/* 채팅 */}
+        <div id="empty-box-forChat" aria-label="빈 박스" />
+        <div className="text-[14px]">
+          {messages.map((msg, idx) => (
+            <ChatBox key={idx} nickname={msg.nickname} message={msg.message} />
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ChatWindow;
 
+type ChatProps = {
+  nickname: string;
+  message: string;
+};
 //채팅 박스
-const ChatBox = () => {
-    return (
-        <div aria-label="chat w-full">
-            <button className="px-[6px] py-[4px] text-left">
-                <span className="mr-[4px] leading-[18px] m-[-2px_0] p-[2px_4px_2px_2px] relative text-green-500">
-                    닉네임
-                </span>
+const ChatBox = ({ nickname, message }: ChatProps) => {
+  return (
+    <div aria-label="chat w-full">
+      <button className="px-[6px] py-[4px] text-left">
+        <span className="mr-[4px] leading-[18px] m-[-2px_0] p-[2px_4px_2px_2px] relative text-green-500">
+          {nickname}
+        </span>
 
-                <span className="text-[#2e3033] text-left break-words leading-[20px]">
-                        ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
-                </span>
-            </button>
-        </div>
-    )
-}
+        <span className="text-[#2e3033] text-left break-words leading-[20px]">
+          {message}
+        </span>
+      </button>
+    </div>
+  );
+};
--- src/app/(route)/live/[id]/page.tsx
+++ src/app/(route)/live/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import useScreenControl from "@/app/_store/live/useScreenControl";
 import LiveStreamWrapper from "./widget/LiveStreamWrapper.client";
 import LiveWrapper from "./widget/Wrapper.clinet";
@@ -7,48 +7,45 @@ import VideoPlayer from "./widget/VideoPlayer.client";
 import ChatLayout from "./widget/Chat.client";
 import { useEffect } from "react";
 
-export default function Page(){
-    const { isFullscreen, offFullScreen} = useScreenControl();
+export default function Page({ params }: { params: { id: string } }) {
+  const { isFullscreen, offFullScreen } = useScreenControl();
 
-    //esc로 전체화면을 강제 해제 했을 때 풀 스크린 관련 state off
-    const clearFullScreenDetect = () => { 
-      if (!document.fullscreenElement) offFullScreen();
-    };
-    
-    useEffect(() => {
-      (async () => {
-        try {
-          // 풀 스크린 false 풀 스크린 중인 요소가 있을 때 해제 요청
-          if (!isFullscreen && document.fullscreenElement) 
-              await document.exitFullscreen();
+  //esc로 전체화면을 강제 해제 했을 때 풀 스크린 관련 state off
+  const clearFullScreenDetect = () => {
+    if (!document.fullscreenElement) offFullScreen();
+  };
 
-          // 풀 스크린 요청
-          else if (isFullscreen) 
-              await document.body.requestFullscreen();
-          
-        } catch (err) {
-          console.error("Error fullscreen:", err);
-        }
-      })();     
-    }, [isFullscreen]);
-  
-    //풀 스크린 이벤트 핸들러
-    useEffect(() => {
-      document.addEventListener("fullscreenchange", clearFullScreenDetect);
-      return () => document.removeEventListener("fullscreenchange", clearFullScreenDetect);
-    }, []);
+  useEffect(() => {
+    (async () => {
+      try {
+        // 풀 스크린 false 풀 스크린 중인 요소가 있을 때 해제 요청
+        if (!isFullscreen && document.fullscreenElement)
+          await document.exitFullscreen();
+        // 풀 스크린 요청
+        else if (isFullscreen) await document.body.requestFullscreen();
+      } catch (err) {
+        console.error("Error fullscreen:", err);
+      }
+    })();
+  }, [isFullscreen]);
 
-    return (
-        <LiveWrapper>
-            {/* 라이브 스트리밍 */}
-            <LiveStreamWrapper>
-              <VideoPlayer/>
-              <LiveDetails/>
-            </LiveStreamWrapper>
+  //풀 스크린 이벤트 핸들러
+  useEffect(() => {
+    document.addEventListener("fullscreenchange", clearFullScreenDetect);
+    return () =>
+      document.removeEventListener("fullscreenchange", clearFullScreenDetect);
+  }, []);
 
-            {/* 채팅창 */}
-            <ChatLayout/> 
+  return (
+    <LiveWrapper>
+      {/* 라이브 스트리밍 */}
+      <LiveStreamWrapper>
+        <VideoPlayer />
+        <LiveDetails />
+      </LiveStreamWrapper>
 
-        </LiveWrapper> 
-    )
+      {/* 채팅창 */}
+      <ChatLayout roomId={params.id} />
+    </LiveWrapper>
+  );
 }
