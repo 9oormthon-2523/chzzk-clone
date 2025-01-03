eaafdfb7c35232781239d86fc5edee59b4e95530
donggyun <donggyunkim12@gmail.com>
2025-01-04 00:34:45 +0900

    feat(studioPage): 팔로워 리스트 컴포넌트 연동
    - FollowInfo, ISODate 타입 정의
    - ISO 시간 변환 함수 formatDate 구현
    
    Resolves: #58

--- src/app/(route)/studio/[uid]/(route)/follower/_components/UserColumn.tsx
+++ src/app/(route)/studio/[uid]/(route)/follower/_components/UserColumn.tsx
@@ -1,28 +1,32 @@
+import { FollowInfo } from '@/app/_types/follow.type';
+import { formatDate } from '@/app/_utils/common/formatDate';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-const UserColumn = () => {
+const UserColumn = (props: FollowInfo) => {
+  const { follower, created_at } = props;
+
   return (
     <tr>
       <td className="p-[12px_20px]">
         <div className="flex gap-[12px] items-center">
           <Image
-            src="/userImage.webp"
+            src={follower.profile_img}
             alt=""
             width={36}
             height={36}
             className="rounded-full"
           />
           <Link
-            href={'/channel/1d8940af-d8ce-43e6-9d59-549f988160ab'}
+            href={`/channel/${follower.uid}`}
             className="text-[#222] text-[15px] relative -top-[2px]"
           >
-            username
+            {follower.nickname}
           </Link>
         </div>
       </td>
-      <td className="text-center text-[13px]">2025-01-02 16:28:07</td>
+      <td className="text-center text-[13px]">{formatDate(created_at)}</td>
       <td className="text-center text-[13px]">18분</td>
       <td className="text-center">
         <button className="font-blackHanSans bg-[#4e41db26] text-[#4e41db] text-[15px] rounded-[5px] py-[4px] px-[15px]">
