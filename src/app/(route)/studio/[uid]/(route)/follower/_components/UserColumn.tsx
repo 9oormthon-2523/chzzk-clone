bbae26b698e11641450642cd103390b42e7a526e
donggyun <donggyunkim12@gmail.com>
2025-01-04 14:36:50 +0900

    feat(studioPage): 팔로우 경과 시간 로직 `elapsedTime` 구현
    moment-timezone 라이브러리를 이용하여 UTC 기준으로 시간을 계산하여 다른 국가에서도 똑같은 결과를 보게 유도 함
    
    Resolves: #58

--- src/app/(route)/studio/[uid]/(route)/follower/_components/UserColumn.tsx
+++ src/app/(route)/studio/[uid]/(route)/follower/_components/UserColumn.tsx
@@ -1,4 +1,5 @@
 import { FollowInfo } from '@/app/_types/follow.type';
+import { elapsedTime } from '@/app/_utils/common/elapsedTime';
 import { formatDate } from '@/app/_utils/common/formatDate';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -26,7 +27,7 @@ const UserColumn = (props: FollowInfo) => {
         </Link>
       </td>
       <td className="text-center text-[13px]">{formatDate(created_at)}</td>
-      <td className="text-center text-[13px]">18분</td>
+      <td className="text-center text-[13px]">{elapsedTime(created_at)}</td>
       <td className="text-center">
         <button className="font-blackHanSans bg-[#4e41db26] text-[#4e41db] text-[15px] rounded-[5px] py-[4px] px-[15px]">
           팔로우
