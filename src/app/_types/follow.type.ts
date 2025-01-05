ae7ecbc8e635bede85ba51aa9065ddb614eeef69
donggyun <donggyunkim12@gmail.com>
2025-01-05 18:20:35 +0900

    feat(studioPage): follow, unfollow 로직 구현 #58

--- src/app/_types/follow.type.ts
+++ src/app/_types/follow.type.ts
@@ -4,10 +4,12 @@ export interface FollowInfo {
   id: number;
   created_at: ISODate;
   status: 'active' | 'block';
-  follower: {
-    uid: string;
-    nickname: string;
-    profile_img: string;
-    is_following_user: boolean;
-  };
+  follower: Follower;
+}
+
+export interface Follower {
+  uid: string;
+  nickname: string;
+  profile_img: string;
+  is_following_user: boolean;
 }
