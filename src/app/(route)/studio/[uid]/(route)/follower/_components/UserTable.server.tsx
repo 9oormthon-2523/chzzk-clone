971343811911c5a138bc578bed4922a0f1da804d
donggyun <donggyunkim12@gmail.com>
2025-01-06 10:51:03 +0900

    design(studioPage): follower 페이지 테이블 스크롤 디자인 적용 #58

--- src/app/(route)/studio/[uid]/(route)/follower/_components/UserTable.server.tsx
+++ src/app/(route)/studio/[uid]/(route)/follower/_components/UserTable.server.tsx
@@ -8,14 +8,14 @@ interface Props {
 
 const UserTable = ({ followers }: Props) => {
   return (
-    <table className="text-[#697183] border-t border-t-[#ddd] border-b border-b-[#ddd] text-[15px] mt-[16px] table-fixed">
+    <table className="block text-[#697183] border-t border-t-[#ddd] border-b border-b-[#ddd] text-[15px] mt-[16px] table-fixed overflow-y-auto h-[495px] w-full border-separate border-spacing-0 scrollbar-thin">
       <colgroup className="font-blackHanSans">
         <col />
         <col className="w-[14.5%] min-w-[82px]" />
         <col className="w-[14.5%]" />
-        <col className="w-[16.5%]" />
+        <col className="w-[16.5%] min-w-[75px]" />
       </colgroup>
-      <thead className="bg-[#f5f6f880] border-b border-b-[#ddd] h-[40px]">
+      <thead className="bg-[#f5f6f8] h-[40px] sticky top-0 z-10">
         <tr>
           <th scope="col">닉네임</th>
           <th scope="col">팔로우 등록일</th>
@@ -24,6 +24,21 @@ const UserTable = ({ followers }: Props) => {
         </tr>
       </thead>
       <tbody>
+        {followers.map((follower) => (
+          <UserColumn key={follower.id} {...follower} />
+        ))}
+        {followers.map((follower) => (
+          <UserColumn key={follower.id} {...follower} />
+        ))}
+        {followers.map((follower) => (
+          <UserColumn key={follower.id} {...follower} />
+        ))}
+        {followers.map((follower) => (
+          <UserColumn key={follower.id} {...follower} />
+        ))}
+        {followers.map((follower) => (
+          <UserColumn key={follower.id} {...follower} />
+        ))}
         {followers.map((follower) => (
           <UserColumn key={follower.id} {...follower} />
         ))}
