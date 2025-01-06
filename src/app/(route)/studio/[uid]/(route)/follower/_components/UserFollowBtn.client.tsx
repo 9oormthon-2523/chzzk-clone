2cb1cc6b44ac5eb93e826e804123815843789dc8
donggyun <donggyunkim12@gmail.com>
2025-01-06 10:51:03 +0900

    design(studioPage): follower 페이지 테이블 스크롤 디자인 적용 #58

--- src/app/(route)/studio/[uid]/(route)/follower/_components/UserFollowBtn.client.tsx
+++ src/app/(route)/studio/[uid]/(route)/follower/_components/UserFollowBtn.client.tsx
@@ -15,7 +15,7 @@ const UserFollowBtn = (props: Props) => {
   if (follower.is_following_user) {
     return (
       <button
-        className="font-blackHanSans bg-[#fff] text-[#697183] text-[15px] rounded-[5px] py-[4px] px-[15px] border border-[#ddd]"
+        className="font-blackHanSans bg-[#fff] text-[#697183] text-[15px] rounded-[5px] py-[4px] px-[15px] border border-[#ddd] box-border w-[70px]"
         onClick={() =>
           unfollow({ uid: follower.uid, nickname: follower.nickname })
         }
@@ -27,7 +27,7 @@ const UserFollowBtn = (props: Props) => {
 
   return (
     <button
-      className="font-blackHanSans bg-[#4e41db26] text-[#4e41db] text-[15px] rounded-[5px] py-[4px] px-[15px]"
+      className="font-blackHanSans bg-[#4e41db26] text-[#4e41db] text-[15px] rounded-[5px] py-[4px] px-[15px] w-[70px]"
       onClick={() => follow({ uid: follower.uid, nickname: follower.nickname })}
     >
       팔로우
