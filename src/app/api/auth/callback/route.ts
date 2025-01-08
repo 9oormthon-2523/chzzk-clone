0d5256d0efbc05d5bf0b8f3c7a9561ca166e1909
donggyun <donggyunkim12@gmail.com>
2025-01-08 15:27:18 +0900

    feat(api/auth/callback): 카카오 로그인 시 public.users 테이블에 데이터 upsert 로직 추가 #65

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -15,8 +15,26 @@ export async function GET(request: Request) {
     // 인증 코드로 세션 교환
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
-    // 오류가 없다면 로그인처리 (redirect)
-    if (!error) return NextResponse.redirect(`${origin}${next}`);
+    // 오류가 없다면
+    // 1. public.users 테이블에 유저 정보 저장
+    // 2. next로 redirect
+
+    if (!error) {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user)
+        await supabase.from('users').upsert({
+          id: user.id,
+          email: user.email,
+          created_at: user.created_at,
+          nickname: user.user_metadata.full_name,
+          profile_img: user.user_metadata.avatar_url,
+          channel_intro: null,
+        });
+      return NextResponse.redirect(`${origin}${next}`);
+    }
   }
 
   // 오류 페이지로 리다이렉트
