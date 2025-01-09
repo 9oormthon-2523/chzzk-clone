8bfc7b9999bca2374e85188a9c41c896c73ba19b
yungan9 <geyun105@naver.com>
2025-01-09 09:36:49 +0900

    feat(channelPage): 채널 프로필 컴포넌트 layout 적용 - 채널 프로필 컴포넌트 layout에 적용 (나머지 페이지에서 제거) - 유저 정보 불러오는 로직 추가

--- src/app/(route)/channel/[uid]/layout.tsx
+++ src/app/(route)/channel/[uid]/layout.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
 import Header from "@/app/_components/Header/Header.server";
 import NavBar from "@/app/(route)/(main)/_components/NavBar/NavBar.client";
 import useNavToggle from "@/app/_store/main/useNavToggle.client";
+import { createClient } from "../../../_utils/supabase/client";
+import ChannelProfile from "@/app/(route)/channel/[uid]/components/ChannelProfile";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -11,12 +14,56 @@ interface RootLayoutProps {
 
 const RootLayout = ({ children }: RootLayoutProps) => {
   const { isOpen } = useNavToggle();
+  const [isClient, setIsClient] = useState(false); 
+  const [userInfo, setUserInfo] = useState<{ nickname: string; channel_intro: string; img_url: string } | null>(null);
+  const { uid } = useParams(); 
+  
+  const fetchUserInfo = async () => {
+    if (!uid) return; 
+    const supabase = createClient();
+    const { data, error } = await supabase
+      .from("users")
+      .select("nickname, channel_intro, profile_img")
+      .eq("id", uid)
+      .single();
+
+    if (error) {
+      console.error("사용자 정보 불러오기 오류", error);
+    } else {
+      setUserInfo({
+        nickname: data.nickname,
+        channel_intro: data.channel_intro,
+        img_url: data.profile_img || "",
+      });
+    }
+  };
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClient && uid) {
+      fetchUserInfo(); 
+    }
+  }, [isClient, uid]);
+
+  if (!userInfo) return <p>로딩 중...</p>;
 
   return (
     <div>
       <Header />
       {isOpen && <NavBar />}
+      <div className="mx-12">
+        <div className="h-32" />
+        <ChannelProfile
+          img_url={userInfo.img_url}
+          nickname={userInfo.nickname}
+          follower={2.4} 
+          context={userInfo.channel_intro}
+        />
       {children}
+      </div>
     </div>
   );
 };
