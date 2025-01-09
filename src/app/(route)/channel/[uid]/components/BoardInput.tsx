22ee42592b940845df391182228254843781d70a
yungan9 <geyun105@naver.com>
2025-01-09 09:49:07 +0900

    feat(channelPage): 글쓰기 페이지 동적라우팅

--- src/app/(route)/channel/[uid]/components/BoardInput.tsx
+++ src/app/(route)/channel/[uid]/components/BoardInput.tsx
@@ -1,17 +1,26 @@
 'use client';
-import { useRouter } from 'next/navigation';
+
+import { useRouter, useParams } from 'next/navigation';
 
 const BoardInput = () => {
-  const router = useRouter(); 
+  const router = useRouter();
+  const { uid } = useParams();
+  
   const useHandleClick = () => {
-    router.push(`/channel/1/write`);
-  }
-  return (
-    <div className="flex h-16 mt-4 w-full bg-gray-200 hover:bg-gray-300 rounded-2xl items-center font-semibold text-gray-500 pl-4 cursor-pointer"
-    onClick={useHandleClick}>
-      <div className="flex flex-col flex-shrink-0 w-10 h-10 rounded-full bg-white shadow-md text-sm mr-2"/>
-    어떤 이야기를 남길건가요?</div>
-  )
-}
+    if (uid) {
+      router.push(`/channel/${uid}/write`); 
+    }
+  };
 
-export default BoardInput
+  return (
+    <div
+      className="flex h-16 mt-4 w-full bg-gray-200 hover:bg-gray-300 rounded-2xl items-center font-semibold text-gray-500 pl-4 cursor-pointer"
+      onClick={useHandleClick}
+    >
+      <div className="flex flex-col flex-shrink-0 w-10 h-10 rounded-full bg-white shadow-md text-sm mr-2"/>
+      어떤 이야기를 남길건가요?
+    </div>
+  );
+};
+
+export default BoardInput;
