4ac15705d6d16f9e362b5cd91a53bcbe58faf301
yungan9 <geyun105@naver.com>
2025-01-09 09:52:56 +0900

    design(channelPage): 글 목록 프로필 이미지 UI 수정

--- src/app/(route)/channel/[uid]/components/Post.tsx
+++ src/app/(route)/channel/[uid]/components/Post.tsx
@@ -4,14 +4,29 @@ interface PostProps {
   nickname: string;
   content: string;
   img_url: string | null;
+  profile_img: string | null;
 }
 
 const Post = (props: PostProps) => {
-  const { nickname, content, img_url } = props;
+  const { nickname, content, img_url, profile_img } = props;
 
   return (
     <div className="flex items-start w-full h-48 p-4 bg-gray-50 rounded-lg">
-      <div className="flex flex-col flex-shrink-0 w-12 h-12 rounded-full bg-white shadow-md text-sm mr-2" />
+      <div className="flex flex-col flex-shrink-0 w-12 h-12 rounded-full bg-white shadow-md text-sm mr-2 relative">
+        {profile_img ? (
+          <Image
+            src={profile_img}
+            alt="프로필 이미지"
+            layout="fill"
+            objectFit="cover" 
+            className="rounded-full"
+          />
+        ) : (
+          <div className="w-full h-full flex justify-center items-center text-gray-500">
+            N/A
+          </div>
+        )}
+      </div>
       
       <div className="flex flex-col flex-wrap">
         <p className="text-m font-black">{nickname}</p>
