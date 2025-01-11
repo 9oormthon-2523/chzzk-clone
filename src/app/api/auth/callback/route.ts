ea4bc001c52531056458eb764e8782988e5f6838
donggyun <donggyunkim12@gmail.com>
2025-01-11 16:00:59 +0900

    fix(login): 로그인 리다이렉트 값 수정 #120

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -6,7 +6,6 @@ export async function GET(request: Request) {
   // URL에서 쿼리 파라미터 가져오기
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code'); // OAuth 인증 코드
-  const next = searchParams.get('next') ?? '/'; // 리다이렉트할 경로
 
   if (code) {
     // Supabase 클라이언트 생성
@@ -33,7 +32,7 @@ export async function GET(request: Request) {
           profile_img: user.user_metadata.avatar_url,
           channel_intro: null,
         });
-      return NextResponse.redirect(`${origin}${next}`);
+      return NextResponse.redirect(`${origin}/`);
     }
   }
 
