19a2818b35b47872a6576b2ab23aad203ab29aef
AhnRian <00rian@naver.com>
2025-01-13 00:39:15 +0900

    feat(mainPage):방송 카드or 프로필 누르면 해당 채널 or 프로필로 이동 및 hover시 오버레이 레이어 추가

--- src/app/(route)/(main)/_components/StreamCard/StreamCard.server.tsx
+++ src/app/(route)/(main)/_components/StreamCard/StreamCard.server.tsx
@@ -28,25 +28,35 @@ const StreamCard = (card: StreamCardProps) => {
   const router = useRouter();
   const defaultImage = "/channelPage/blank_profile.svg";
 
-  const handleCardClick = () => {
+  const moveToLivePage = () => {
     router.push(`/live/${uid}`);
   };
-
+  const moveToProfile = () => {
+    router.push(`/channel/${uid}`);
+  };
   return (
-    <div
-      className="hover:cursor-pointer bg-white rounded-lg shadow-md p-2 w-80"
-      onClick={handleCardClick}
-    >
+    <div className="hover:cursor-pointer bg-white rounded-lg p-2 w-80 ">
       {/* 이미지 */}
       <div
-        className="relative h-44 rounded-lg border border-gray-200"
-        style={{
-          backgroundImage: `url(${thumbnail || "/mainPage/thumbnailImg.png"})`,
-          backgroundPosition: "center center",
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-        }}
+        className="relative h-44 rounded-lg border border-gray-200 hover:cursor-pointer"
+        onClick={moveToLivePage}
       >
+        {/* 이미지 배경 */}
+        <div
+          className="absolute inset-0 rounded-lg"
+          style={{
+            backgroundImage: `url(${
+              thumbnail || "/mainPage/thumbnailImg.png"
+            })`,
+            backgroundPosition: "center center",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+          }}
+        />
+
+        {/* 오버레이 레이어 추가 */}
+        <div className="absolute inset-0 bg-black opacity-0 hover:opacity-25 transition-opacity rounded-lg"></div>
+
         {is_active && (
           <div className="absolute top-2 left-2 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded">
             LIVE
@@ -59,9 +69,17 @@ const StreamCard = (card: StreamCardProps) => {
       </div>
 
       <div className="p-2">
-        <h2 className="text-sm font-semibold line-clamp-1">{title}</h2>
-
-        <div className="flex items-center gap-1 mt-1">
+        <h2
+          className="text-sm font-semibold line-clamp-1"
+          onClick={moveToLivePage}
+        >
+          {title}
+        </h2>
+        {/* 프로필 */}
+        <span
+          className="inline-flex items-center gap-1 mt-1"
+          onClick={moveToProfile}
+        >
           <div className="relative w-6 h-6 rounded-full overflow-hidden border border-gray-300">
             <Image
               src={profile_img || defaultImage}
@@ -71,8 +89,8 @@ const StreamCard = (card: StreamCardProps) => {
               className="rounded-full"
             />
           </div>
-          <p className="text-xs text-gray-700 font-medium">{nickname}</p>
-        </div>
+          <span className="text-xs text-gray-700 font-medium">{nickname}</span>
+        </span>
 
         <div className="flex flex-wrap gap-1 mt-2">
           {tags.length > 0 &&
