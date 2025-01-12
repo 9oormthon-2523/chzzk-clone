4f1e6a9e002e666095a212f64eaf7e56bc4ea027
AhnRian <00rian@naver.com>
2025-01-13 00:06:14 +0900

    fix(mainPage): 헤더 메뉴바 메인페이지에만 추가

--- src/app/(route)/(main)/_components/StreamCard/StreamCard.server.tsx
+++ src/app/(route)/(main)/_components/StreamCard/StreamCard.server.tsx
@@ -15,8 +15,16 @@ interface StreamCardProps {
 }
 
 const StreamCard = (card: StreamCardProps) => {
-  const { title, nickname, audience_cnt, uid, thumbnail, profile_img, tags } =
-    card;
+  const {
+    title,
+    nickname,
+    audience_cnt,
+    uid,
+    thumbnail,
+    is_active,
+    profile_img,
+    tags,
+  } = card;
   const router = useRouter();
   const defaultImage = "/channelPage/blank_profile.svg";
 
@@ -39,9 +47,11 @@ const StreamCard = (card: StreamCardProps) => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <div className="absolute top-2 left-2 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded">
-          LIVE
-        </div>
+        {is_active && (
+          <div className="absolute top-2 left-2 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded">
+            LIVE
+          </div>
+        )}
 
         <div className="absolute top-2 right-2 bg-black text-white text-xs font-semibold px-2 py-1 rounded">
           {audience_cnt.toLocaleString()}명
@@ -52,7 +62,7 @@ const StreamCard = (card: StreamCardProps) => {
         <h2 className="text-sm font-semibold line-clamp-1">{title}</h2>
 
         <div className="flex items-center gap-1 mt-1">
-          <div className="relative w-6 h-6 rounded-full overflow-hidden">
+          <div className="relative w-6 h-6 rounded-full overflow-hidden border border-gray-300">
             <Image
               src={profile_img || defaultImage}
               alt={`${nickname} profile`}
--- src/app/(route)/(main)/page.tsx
+++ src/app/(route)/(main)/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import Header from "../../_components/Header/Header.server";
 import StreamList from "./_components/StreamList/StreamList.server";
+import NavBar from "./_components/NavBar/NavBar.client";
+
 export default function Home() {
   return (
     <div>
       <Header />
+      <NavBar />
       <div className="text-[22px] py-[60px] pl-[60px] pr-[20px]">
         <div className="px-4 pt-4 flex justify-between">
           <strong className="font-blackHanSans font-thin ">인기 Live</strong>
