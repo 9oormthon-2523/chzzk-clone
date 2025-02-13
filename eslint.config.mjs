8c6affdb8535a8feb4dc4c8a26da239cbdd42ffd
donggyun <donggyunkim12@gmail.com>
2025-02-13 20:47:31 +0900

    chore(eslint): eslint 업데이트, eslint-plugin-prettier 의존성 설치 #149

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,11 +13,30 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
+    plugins: {
+      prettier,
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
     },
-    extends: ["eslint:recommended","prettier"]
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    ignores: ['**/*.config.js', '!**/eslint.config.js'],
+    settings: {
+      // 현재 React 버전을 명시
+      react: {
+        version: '19.0',
+      },
+    },
   },
 ];
 
