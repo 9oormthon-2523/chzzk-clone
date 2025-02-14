a718e6c89f62c5d942f874cb6aa3a618863ba550
donggyun <donggyunkim12@gmail.com>
2025-02-14 10:24:14 +0900

    design(studio): NoticeItem 스타일링 변경 #172

--- src/app/(route)/studio/[uid]/(route)/(dashboard)/_components/NoticeItem.tsx
+++ src/app/(route)/studio/[uid]/(route)/(dashboard)/_components/NoticeItem.tsx
@@ -7,14 +7,12 @@ const NoticeItem = (props: ListItemProps) => {
   const { date, title } = props;
 
   return (
-    <li className="m-0 p-0 box-border w-auto text-[13px]" itemType="">
-      <div className="grid grid-flow-col items-center p-[0px_8px_7px_10px] w-full leading-5 tracking-[-.5px] group">
-        <strong className="text-[#222] flex-grow min-w-0 overflow-hidden text-ellipsis whitespace-nowrap font-normal decoration-[0.5px]">
-          {title}
-        </strong>
+    <div className="mb-1 w-auto text-[13px]">
+      <div className="grid grid-flow-col items-center w-full leading-5 tracking-[-.5px] group">
+        <strong className="text-[#222] flex-grow min-w-0 overflow-hidden font-normal">{title}</strong>
         <div className="text-[#697183] flex-none leading-[18px] ml-auto pl-8">{date}</div>
       </div>
-    </li>
+    </div>
   );
 };
 
