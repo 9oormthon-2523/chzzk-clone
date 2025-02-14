14236cc12d3b71d7fed22cfc8143a4193a7d3393
ki2183 <ki2183@naver.com>
2025-02-14 14:55:48 +0900

    feat(livePage/render): 전역 상태 타입 선언
    - 추후 업데이트 될 supabase-cli에 들어갈 데이터 일부를 미리 가져와 타입선언
    - pr을 올리고 나중에 database.type.ts의 타입을 가져와서 사용 예정
    
    Resolve #162

--- src/app/_types/live/liveType.ts
+++ src/app/_types/live/liveType.ts
@@ -1,3 +1,108 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+// PR 전이라 임시 type 가져옴옴
+export type Database = {
+  public: {
+    Tables: {
+      streaming_rooms: {
+        Row: {
+          audience_cnt: number
+          category: string | null
+          is_active: boolean
+          nickname: string | null
+          start_time: string | null
+          tags: string[]
+          thumbnail: string | null
+          title: string | null
+          uid: string
+        }
+        Insert: {
+          audience_cnt?: number
+          category?: string | null
+          is_active?: boolean
+          nickname?: string | null
+          start_time?: string | null
+          tags?: string[]
+          thumbnail?: string | null
+          title?: string | null
+          uid: string
+        }
+        Update: {
+          audience_cnt?: number
+          category?: string | null
+          is_active?: boolean
+          nickname?: string | null
+          start_time?: string | null
+          tags?: string[]
+          thumbnail?: string | null
+          title?: string | null
+          uid?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "streaming_rooms_uid_fkey1"
+            columns: ["uid"]
+            isOneToOne: true
+            referencedRelation: "follower_details"
+            referencedColumns: ["follower_uid"]
+          },
+          {
+            foreignKeyName: "streaming_rooms_uid_fkey1"
+            columns: ["uid"]
+            isOneToOne: true
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      users: {
+        Row: {
+          channel_intro: string | null
+          created_at: string
+          email: string | null
+          id: string
+          nickname: string | null
+          profile_img: string | null
+        }
+        Insert: {
+          channel_intro?: string | null
+          created_at?: string
+          email?: string | null
+          id?: string
+          nickname?: string | null
+          profile_img?: string | null
+        }
+        Update: {
+          channel_intro?: string | null
+          created_at?: string
+          email?: string | null
+          id?: string
+          nickname?: string | null
+          profile_img?: string | null
+        }
+        Relationships: []
+      }
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+// 스트리밍 룸 DTO_TYPE
+export type StreamingRoomState = 
+    Database["public"]["Tables"]["streaming_rooms"]["Row"];
+
+// 호스트 DTO_TYPE
+export type HostInfoState = 
+    Pick<Database["public"]["Tables"]["users"]["Row"], "id" | "profile_img" | "nickname">;
+
 export type getRoomDtoType = {
     uid:string
     title:string | null
