4a94fc1aa3c299fdeb31b8cc3107f3e9936e5c47
AhnRian <00rian@naver.com>
2025-02-17 15:05:18 +0900

    feat: 채팅 글자수 제한

--- src/app/(route)/live/[host_uid]/components/Chat/Input.client.tsx
+++ src/app/(route)/live/[host_uid]/components/Chat/Input.client.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import SvgIcon from "@/app/_components/SVGIcon.server";
+import React, { useState } from 'react';
+import SvgIcon from '@/app/_components/SVGIcon.server';
 /**
  * 채팅 입력란
  */
@@ -7,52 +7,47 @@ type MessageInputProps = {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   onSend: () => void;
-  client_uid: string|undefined
+  client_uid: string | undefined;
 };
 const ChatInput = ({ value, onChange, onSend, client_uid }: MessageInputProps) => {
   const [isComposing, setIsComposing] = useState(false);
-  const onPressEnter = (e:React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === "Enter") {
+  const onPressEnter = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
       e.preventDefault();
-      if(navigator.userAgent.includes("Firefox")) {
-        if(!e.nativeEvent.isComposing) {
+      if (navigator.userAgent.includes('Firefox')) {
+        if (!e.nativeEvent.isComposing) {
           onSend();
         }
-      }
-      else if(!isComposing) {
+      } else if (!isComposing) {
         onSend();
       }
     }
-  }
+  };
   return (
-    <div
-      id="live-chatting-area"
-      className="flex-none p-[10px_20px] relative z-[30]"
-    >
+    <div id="live-chatting-area" className="flex-none p-[10px_20px] relative z-[30]">
       <div className="items-center bg-[#f5f5f5] rounded-[8px] flex p-[5px] relative w-full">
         <button className="items-center flex-none h-[30px] relative w-[30px]">
           <i className="bg-[#0000001a] rounded-[5px] text-[#2e3033] inline-block h-[24px] p-[2px] align-top w-[24px]">
             <SvgIcon name="ChatSetting" width={20} height={20} />
           </i>
         </button>
-        <textarea
-          aria-label="채팅 입력"
-          disabled={!client_uid}
-          style={{ cursor : client_uid ? undefined : "not-allowed" }}
-          className="!h-[40px] m-[-10px_0_-10px_4px] p-[10px_0] bg-transparent border-[0px] text-[#2e3033] max-h-[60px] min-h-[20px] outline-none overflow-y-auto relative resize-none whitespace-normal w-full"
-          value={value}
-          onChange={onChange}
-          onKeyDown={onPressEnter}
-          onCompositionStart={() => {
-            if(navigator.userAgent.includes("Firefox")) return;
-            setIsComposing(true)
-          }}
-          onCompositionEnd={() => {
-            if(navigator.userAgent.includes("Firefox")) return;
-            setIsComposing(false)}
-          }
-          placeholder="채팅을 입력해주세요"
-        />
+        <div className="flex w-full items-center">
+          <textarea
+            aria-label="채팅 입력"
+            disabled={!client_uid}
+            style={{ cursor: client_uid ? undefined : 'not-allowed' }}
+            className="h-[40px] p-[10px] bg-transparent border-[0px] text-[#2e3033] max-h-[60px] min-h-[20px] outline-none overflow-hidden resize-none whitespace-normal flex-grow"
+            value={value}
+            onChange={onChange}
+            onKeyDown={onPressEnter}
+            onCompositionStart={() => setIsComposing(true)}
+            onCompositionEnd={() => setIsComposing(false)}
+            placeholder="채팅을 입력해주세요"
+            maxLength={100}
+          />
+
+          <span className="w-full text-right text-sm text-gray-400 mt-1">{value.length}/100</span>
+        </div>
       </div>
 
       <div className="items-center flex mt-[10px] p-[2px_0] relative">
--- src/app/_components/Chat/MessageInput.tsx
+++ src/app/_components/Chat/MessageInput.tsx
@@ -4,19 +4,11 @@ type MessageInputProps = {
   onSend: () => void;
 };
 
-export default function MessageInput({
-  value,
-  onChange,
-  onSend,
-}: MessageInputProps) {
+export default function MessageInput({ value, onChange, onSend }: MessageInputProps) {
   return (
     <div>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-        placeholder="채팅을 입력해주세요"
-      />
+      <input type="text" value={value} onChange={onChange} placeholder="채팅을 입력해주세요1" />
+
       <button onClick={onSend}>전송</button>
     </div>
   );
