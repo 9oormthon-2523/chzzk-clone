00bebbfc22650df6a2209bb1a147316f001323c6
AhnRian <00rian@naver.com>
2025-02-18 17:19:06 +0900

    fix: textarea->input 변경 및 form 태그 추가

--- src/app/(route)/live/[host_uid]/components/Chat/Input.client.tsx
+++ src/app/(route)/live/[host_uid]/components/Chat/Input.client.tsx
@@ -5,50 +5,41 @@ import SvgIcon from '@/app/_components/SVGIcon.server';
  */
 type MessageInputProps = {
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onSend: () => void;
   client_uid: string | undefined;
 };
 const ChatInput = ({ value, onChange, onSend, client_uid }: MessageInputProps) => {
-  const [isComposing, setIsComposing] = useState(false);
-  const onPressEnter = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      if (navigator.userAgent.includes('Firefox')) {
-        if (!e.nativeEvent.isComposing) {
-          onSend();
-        }
-      } else if (!isComposing) {
-        onSend();
-      }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (value.length > 0) {
+      onSend();
     }
   };
+
   return (
     <div id="live-chatting-area" className="flex-none p-[10px_20px] relative z-[30]">
-      <div className="items-center bg-[#f5f5f5] rounded-[8px] flex p-[5px] relative w-full">
-        <button className="items-center flex-none h-[30px] relative w-[30px]">
+      <form onSubmit={handleSubmit} className="items-center bg-[#f5f5f5] rounded-[8px] flex p-[5px] relative w-full">
+        <button type="button" className="items-center flex-none h-[30px] relative w-[30px]">
           <i className="bg-[#0000001a] rounded-[5px] text-[#2e3033] inline-block h-[24px] p-[2px] align-top w-[24px]">
             <SvgIcon name="ChatSetting" width={20} height={20} />
           </i>
         </button>
         <div className="flex w-full items-center">
-          <textarea
+          <input
+            type="text"
             aria-label="채팅 입력"
             disabled={!client_uid}
             style={{ cursor: client_uid ? undefined : 'not-allowed' }}
-            className="h-[40px] p-[10px] bg-transparent border-[0px] text-[#2e3033] max-h-[60px] min-h-[20px] outline-none overflow-hidden resize-none whitespace-normal flex-grow "
+            className="h-[40px] p-[10px] bg-transparent border-[0px] text-[#2e3033] outline-none overflow-hidden flex-grow"
             value={value}
             onChange={onChange}
-            onKeyDown={onPressEnter}
-            onCompositionStart={() => setIsComposing(true)}
-            onCompositionEnd={() => setIsComposing(false)}
             placeholder="채팅을 입력해주세요"
             maxLength={100}
           />
-
           <span className="w-[50px] text-right text-sm text-gray-400 mt-1">{value.length}/100</span>
         </div>
-      </div>
+      </form>
 
       <div className="items-center flex mt-[10px] p-[2px_0] relative">
         <button
